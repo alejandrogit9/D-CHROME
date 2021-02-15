f80cb6fa4feeefc1f43a8aca4c2fc860c1f1dfeb
alejandrogit9 <71609561+alejandrogit9@users.noreply.github.com>
2021-02-15 10:28:14 -0600

    Update stage2.py

--- stage2.py
+++ stage2.py
@@ -13,32 +13,32 @@ def test():
     password = "PASS"
 
     mega = Mega()
-    # mega = Mega({'verbose': True})  # verbose option for print output
+    
 
-    # login
+    
     m = mega.login(email, password)
 
-    # get user details
+    
     details = m.get_user()
     print(details)
 
-    # get account files
+    
     files = m.get_files()
 
-    # get account disk quota in MB
+    
     print((m.get_quota()))
-    # get account storage space
+    
     print((m.get_storage_space()))
 
-    # example iterate over files
+    
     for file in files:
         print((files[file]))
 
-    # upload file
+    
     print((m.upload(filename='dll_dat.zip',
                     dest_filename=f'examples_{unique}.zip')))
 
-    # search for a file in account
+    
     file = m.find(f'examples_{unique}.zip')
 
     
